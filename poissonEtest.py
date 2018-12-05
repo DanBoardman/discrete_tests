c44f7f4ee395c39b1895a3c6da634027ab455901
DanBoardman <dan.s.boardman@gmail.com>
2018-12-04 22:41:28 -0500

    Update poissonEtest.py

--- poissonEtest.py
+++ poissonEtest.py
@@ -1,8 +1,4 @@
 """
-
-Project description
-Motivation: You're looking at foot traffic counts from two available storefronts. One is higher, but the samples were taken over just one week and you're not sure how confident to be in the apparent difference. The storefront that more people passed also costs more to rent and if you're wrong it could make it harder to be profitable and cost your job at the company. If only you knew the odds!
-
 Inspiration: Shameless MATLAB Port of Krishnamoorthy's "Two Poisson Means" fortran code. https://github.com/leiferlab/testPoisson
 
 Original inspiration:  5. Two Poisson Means, 6. Power: Two Poisson Means from http://www.ucs.louisiana.edu/~kxk4695/StatCalc.htm
